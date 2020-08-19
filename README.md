93b11ae23142e28ecb628dfb66596b85a0912e04
long.woo <longwunet@outlook.com>
2020-08-19 14:25:49 +0800

    feat: 优化代码

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![NPM VERSION](https://img.shields.io/npm/v/convert-es5-plugin.svg?style=flat)](https://npmjs.com/convert-es5-plugin)
 
-一个将 es6+ 语法转换成 es5 的 webpack 插件
+一个将 es6+ 语法转换成 es5 的 webpack 插件。
 
 ## 安装
 
--- src/convert-es5-plugin.ts
+++ src/convert-es5-plugin.ts
@@ -1,25 +1,71 @@
 import fs from 'fs';
 import acorn from 'acorn';
-import { Compiler } from 'webpack';
-import { ConcatSource } from 'webpack-sources'
+import { Compiler, compilation as webpackCompilation } from 'webpack';
+import { RawSource } from 'webpack-sources'
 import { version } from '../package.json';
 
 class ConvertES5Plugin {
   private readonly pluginName = 'ConvertES5Plugin'
 
+  /**
+   * 优化 js 资源。分析 js 语法，将 es6+ 不兼容低版本浏览器的语法，通过 babel 转换成 es5
+   * @param chunks - 所有文件块
+   * @param compilation - 编译
+   */
+  private optimizeJSChunkAssets(chunks: Array<webpackCompilation.Chunk>, compilation: webpackCompilation.Compilation) {
+    chunks.map(chunk => {
+      chunk.files.map(file => {
+        if (!/\.(m?)js/i.test(file)) return;
+
+        let transformFile;
+        const code = new RawSource(compilation.assets[file]).source();
+
+        // 检测语法是否为 es5
+        console.log('\n')
+        console.log(`🔍 [${file}] 分析语法...`);
+        try {
+          acorn.parse(code, { ecmaVersion: 5 });
+        } catch (err) {
+          console.log(`🚗 [${file}] 存在 ES6+ 的语法，正在转换...`);
+          transformFile = code;
+        }
+
+        if(!transformFile) {
+          console.log(`✅ [${file}] 无需转换`);
+          return;
+        }
+
+        // 使用 babel 将语法转换成 es5
+        const output = require('@babel/core').transformSync(code, {
+          // filename: file,
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: '> 1%, last 2 versions, not ie <= 8'
+              }
+            ]
+          ]
+        });
+
+        compilation.assets[file] = new RawSource(output.code);
+        console.log(`✅ [${file}] 转换成功\n`);
+      });
+    });
+  }
+
   apply(compiler: Compiler) {
-    const { devtool } = compiler.options;
+    // const { devtool } = compiler.options;
 
     compiler.hooks.compilation.tap(this.pluginName, compilation => {
-      if(devtool === 'source-map' || devtool === 'cheap-source-map') {
-        compilation.hooks.buildModule.tap(this.pluginName, mod => {
-          mod.useSourceMap = true;
-        });
-      }
+      // if(devtool === 'source-map' || devtool === 'cheap-source-map') {
+      //   compilation.hooks.buildModule.tap(this.pluginName, mod => {
+      //     mod.useSourceMap = true;
+      //   });
+      // }
 
       // 重新生成 contenthash
       const { mainTemplate } = compilation;
-
       mainTemplate.hooks.hashForChunk.tap(this.pluginName, (hash, chunk) => {
         hash.update(this.pluginName);
         hash.update(JSON.stringify({
@@ -27,48 +73,9 @@ class ConvertES5Plugin {
         }));
       });
 
+      // 处理输出资源
       compilation.hooks.optimizeChunkAssets.tapAsync(this.pluginName, (chunks, callback) => {
-        chunks.map(chunk => {
-          chunk.files.map(file => {
-            if (!/\.(m?)js/i.test(file)) return;
-
-            let transformFile;
-            const code = new ConcatSource(compilation.assets[file]).source();
-
-            // 检测语法是否为 es5
-            console.log(`🔍 [${file}] 分析语法...`);
-            try {
-              acorn.parse(code, { ecmaVersion: 5 });
-            } catch (err) {
-              console.log(`🚗 [${file}] 存在 ES6+ 的语法，正在转换...`);
-              transformFile = code;
-            }
-
-            if(!transformFile) {
-              console.log(`✅ [${file}] 无需转换`);
-              return;
-            }
-
-            // 使用 babel 将语法转换成 es5
-            const output = require('@babel/core').transformSync(code, {
-              filename: file,
-              presets: [
-                [
-                  '@babel/preset-env',
-                  {
-                    targets: '> 1%, last 2 versions, not ie <= 8'
-                  }
-                ]
-              ],
-              compact: false,
-              minified: false,
-              sourceMaps: false
-            });
-
-            compilation.assets[file] = new ConcatSource(output.code);
-            console.log(`✅ [${file}] 转换成功`);
-          });
-        });
+        this.optimizeJSChunkAssets(chunks, compilation);
         callback();
       });
     });
